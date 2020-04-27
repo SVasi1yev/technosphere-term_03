25ed602aa25fcc4f9b6e79eca5f530f988de35a4
SVasi1yev <vsemenm@gmail.com>
2020-04-27 16:22:02 +0300

    -

---
+++ Information retrieval 2/homeworks/04_behavioral_rank/src/main/java/Utils.java
@@ -0,0 +1,13 @@
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.util.HashMap;
+//
+//public class Utils {
+//    public static <K, V> void mapFromTSV(BufferedReader reader, HashMap<K, V> map) throws IOException {
+//        String line = reader.readLine();
+//        while (line != null) {
+//            String[] splited = line.split("\t");
+//            map.put(splited)
+//        }
+//    }
+//}
